fec6a8ee5406ee20902a1f10384bf5ba651ec8ac
tjunjet <latyun142@gmail.com>
2023-07-03 03:00:02 +0000

    A custom message for the commit

---
+++ dist/downloaded_wheel/lib/testWheel/__init__.py
@@ -0,0 +1,4 @@
+def function_init():
+ print('Successfully Imported Init.py')
+def print_age(age):
+ print(f'I am {age} years old')
---
+++ dist/downloaded_wheel/lib/testWheel/test.py
@@ -0,0 +1,21 @@
+from testWheel.__init__ import * 
+def func_test(): 
+    print("Successfully Imported test.py file") 
+def print_name(name): 
+    print(f'Hello {name}') 
+def print_something():
+    print("something")
+def print_fangchen():
+    print("I am Fang Chen")
+def print_jingneng():
+    print("Jing neng is handsome")
+def print_test_age(age): 
+    print_age(age)
+def print_menghan():
+    print("I am Meng Han")
+def print_kaijun():
+    print("I am Kai Jun")
+def print_greg():
+    print("I am greg")
+def print_tj():
+    print("tj"):
